368ed7c555693c54f293b34a6cfe105b3e843ade
nickoval <mykovalmail@gmail.com>
2022-11-07 15:56:39 +0200

    xz

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  searchForm : document.querySelector('#search-form'),
+};
+
+refs.searchForm.addEventListener('submit', (e)=>{})
+
+
+function onSearchForm(evt) {
+  fetchContent()
+}
