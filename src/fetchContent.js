247b8eb0e98325928f73125dcd8d7df707ce276d
nickoval <mykovalmail@gmail.com>
2022-11-07 23:41:05 +0200

    hto-zna-sho

--- src/fetchContent.js
+++ src/fetchContent.js
@@ -1,3 +1,27 @@
-export function name(params) {
-  
+// import axios from 'axios';
+// const axios = require('axios');
+const axios = require('axios').default;
+
+const BASE_URL = 'https://pixabay.com/api/';
+
+// export function fetchContent(name) {
+//   fetch(
+//     `${BASE_URL}?key=31160684-b0388cec495519ac32683cd6a&q=${name}&image_type=photo`
+//   ).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.atatus);
+//     }
+//     return response.json;
+//   });
+// }
+
+export async function fetchContent(name) {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}?key=31160684-b0388cec495519ac32683cd6a&q=${name}&image_type=photo`
+    );
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchContent } from './fetchContent';
+// const axios = require('axios');
+
+// const BASE_URL = 'https://pixabay.com/api/';
 
 const refs = {
-  searchForm : document.querySelector('#search-form'),
+  searchForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  // searchQuery: document.querySelector('[name="searchQuery"]'),
 };
 
-refs.searchForm.addEventListener('submit', (e)=>{})
-
+refs.searchForm.addEventListener('submit', onSearchForm);
 
 function onSearchForm(evt) {
-  fetchContent()
+  evt.preventDefault();
+  // if (evt.target.value.trim() === '') {
+  //   return clearAll();
+  // }
+  // fetchContent(evt.target.value.trim().toLowerCase());
+  console.log(evt.currentTarget.elements.searchQuery.value.toLowerCase());
+  fetchContent(evt.currentTarget.elements.searchQuery.value.toLowerCase());
+}
+
+async function fetchContent(name) {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}?key=31160684-b0388cec495519ac32683cd6a&q=${name}&image_type=photo`
+    );
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
 }
